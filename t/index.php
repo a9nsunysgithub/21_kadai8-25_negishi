bda03dc1200973a2df97c3d8d5cbe11ce9815de2
a9nsunysgithub <31178779+a9nsunysgithub@users.noreply.github.com>
2017-09-28 04:13:08 +0900

    Update index.php

--- t/index.php
+++ t/index.php
@@ -1,34 +1,3 @@
-<?php
-//1.  DB接続します
-try {
-  $pdo = new PDO('mysql:dbname=kadai_db;charset=utf8;host=localhost','root','');
-} catch (PDOException $e) {
-  exit('DbConnectError:'.$e->getMessage());
-}
-
-
-//２．データ登録SQL作成
-$stmt = $pdo->prepare("SELECT * FROM kadai_table");
-$status = $stmt->execute();
-
-//３．データ表示
-$view="";
-if($status==false){
-  //execute（SQL実行時にエラーがある場合）
-  $error = $stmt->errorInfo();
-  exit("ErrorQuery:".$error[2]);
-
-}else{
-  //Selectデータの数だけ自動でループしてくれる
-  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $view = $result["indate"];
-    $view = $result["id"];
-    $view = $result["naiyou"];
-  }
-
-}
-?>
-
 
 <!DOCTYPE html>
 <html lang="ja">
